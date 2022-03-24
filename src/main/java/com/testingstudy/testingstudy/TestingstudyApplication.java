2552c3582b36bde2ae3105c116e62983290810ad
Kaan010 <kaankalan@outlook.com.tr>
2022-03-24 18:52:56 +0300

    Initial commit

---
+++ src/main/java/com/testingstudy/testingstudy/TestingstudyApplication.java
@@ -0,0 +1,13 @@
+package com.testingstudy.testingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testingstudy/testingstudy/TestingstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testingstudy.testingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
